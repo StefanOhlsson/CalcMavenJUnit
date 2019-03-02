6a35ea041b7c48157ae2a57ed14217631a95afe4
StefanOhlsson <stefan.ohlsson.777@gmail.com>
2019-03-02 21:21:30 +0100

    Updated methods in calculatorTest, updated .gitignore

--- src/test/java/com/mycompany/calcmavenjunit/CalculatorTest.java
+++ src/test/java/com/mycompany/calcmavenjunit/CalculatorTest.java
@@ -1,10 +1,7 @@
 package com.mycompany.calcmavenjunit;
 
-import org.junit.After;
-import org.junit.AfterClass;
-import org.junit.Before;
-import org.junit.BeforeClass;
 import org.junit.Test;
+
 import static org.junit.Assert.*;
 
 /**
@@ -13,64 +10,46 @@ import static org.junit.Assert.*;
  */
 public class CalculatorTest {
 
-    public CalculatorTest() {
-    }
+	Calculator calculator = new Calculator();
 
-    /**
-     * Test of add method, of class Calculator.
-     */
-    @Test
-    public void testAdd() {
-        System.out.println("add");
-        int term1 = 4;
-        int term2 = 5;
-        Calculator instance = new Calculator();
-        int expResult = 9;
-        int result = instance.add(term1, term2);
-        assertEquals(expResult, result);
-    }
+	public CalculatorTest() {
+	}
 
-    /**
-     * Test of subtract method, of class Calculator.
-     */
-    @Test
-    public void testSubtract() {
-        System.out.println("subtract");
-        int term1 = 9;
-        int term2 = 4;
-        Calculator instance = new Calculator();
-        int expResult = 5;
-        int result = instance.subtract(term1, term2);
-        assertEquals(expResult, result);
-    }
+	/**
+	 * Test of add method, of class Calculator.
+	 */
+	@Test
+	public void testAdd() {
+		System.out.println("add");
+		assertEquals(98, calculator.add(88, 10));
+	}
 
-    /**
-     * Test of multiply method, of class Calculator.
-     */
-    @Test
-    public void testMultiply() {
-        System.out.println("multiply");
-        int term1 = 7;
-        int term2 = 7;
-        Calculator instance = new Calculator();
-        int expResult = 49;
-        int result = instance.multiply(term1, term2);
-        assertEquals(expResult, result);
-    }
+	/**
+	 * Test of subtract method, of class Calculator.
+	 */
+	@Test
+	public void testSubtract() {
+		System.out.println("subtract");
+		assertEquals(0, calculator.subtract(2, 2));
+	}
 
-    /**
-     * Test of divide method, of class Calculator.
-     */
-    @Test
-    public void testDivide() {
-        System.out.println("divide");
-        int term1 = 8;
-        int term2 = 4;
-        Calculator instance = new Calculator();
-        int expResult = 2;
-        int result = instance.divide(term1, term2);
-        assertEquals(expResult, result);
+	/**
+	 * Test of multiply method, of class Calculator.
+	 */
+	@Test
+	public void testMultiply() {
+		System.out.println("multiply");
+		assertEquals(12, calculator.multiply(2, 6));
+	}
 
-    }
+	/**
+	 * Test of divide method, of class Calculator.
+	 */
+	@Test
+	public void testDivide() {
+		System.out.println("divide");
+		assertEquals(1, calculator.divide(2, 2));
+
+	}
 
 }
